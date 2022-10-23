ed6a1d6e130c24546520d587d86e4df7db9aad5e
miss-mad <mmlh.harris@gmail.com>
2022-10-23 18:22:02 -0400

    added middleware and one more route to server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require("path");
 // the port we want the app to listen to
 const PORT = 3001;
 
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // tells express that the public folder is a static folder that should only be used for GET requests
 // also where we'll send over files from (html, css, images) all in public folder
 app.use(express.static("public"));
@@ -21,6 +25,14 @@ app.get("/send", (req, res) => {
 // another GET request, sending another html file
 app.get("/routes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/routes.html"));
+});
+
+// route to send back json
+// express is listening to /api specifically for GET
+// when it GETs something, it calls the function we give it and then pass it the request and the response
+// express will build the request object and the response object and give it to us
+app.get("/api", (req, res) => {
+  res.json({ term: "api", description: "definition" });
 });
 
 // starts the server by listening at the PORT #
